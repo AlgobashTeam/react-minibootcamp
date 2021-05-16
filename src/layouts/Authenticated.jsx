4e2cf8a67da95f61901e6fc6239d96b32a825228
Sch39 <sch40071807@gmail.com>
2021-05-16 08:15:51 +0700

    finish edit fiture

--- src/layouts/Authenticated.jsx
+++ src/layouts/Authenticated.jsx
@@ -1,40 +1,43 @@
-import { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Redirect, useHistory } from 'react-router-dom';
 import axios from 'axios';
 
-export default function Authenticated (props) {
-  //const [redirectPath, setRedirectPath] = useState('');
+export default function Authenticated(props) {
+  const [redirectPath, setRedirectPath] = useState('');
   const history = useHistory();
 
-  async function checkAuthentication () {
+  async function checkAuthentication() {
     const res = await axios.post('https://jsonplaceholder.typicode.com/posts', {
       title: 'email@gmail.com',
       body: 'Password123123',
     });
 
-    if (!res) {
-      //setRedirectPath('/login');
+    if (res.status !== 200 && res.status !== 201) {
+      setRedirectPath('/login');
       history.push('/login');
+      console.log('diset ke login');
     }
+    // console.log(res.data);
+    // console.log(res);
     return res.data;
   }
 
   // DidMount check authentication.
   // If no authenticated, redirect to Login
-  useEffect( () => {
+  useEffect(() => {
     checkAuthentication()
-  }, []);
+  });
 
-  //if (redirectPath) {
-    //return (
-      //<Redirect
-        //to={{
-          //pathname: "/login",
-          //state: { from: "/" }
-        //}}
-      ///>
-    //);
-  //}
+  if (redirectPath) {
+    return (
+      <Redirect
+        to={{
+          pathname: "/login",
+          state: { from: "/" }
+        }}
+      />
+    );
+  }
 
   return (
     <>
--- src/pages/EditBlog.jsx
+++ src/pages/EditBlog.jsx
@@ -1,10 +1,134 @@
 import Authenticated from '../layouts/Authenticated';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 
 export default function EditBlog() {
+
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const { id } = useParams();
+
+  const [status, setStatus] = useState({
+    loading: false,
+    success: false,
+  });
+
+  // buat tempat penyimpanan single data
+  const [blogData, setBlogData] = useState({});
+
+  // Ambil data dan simpan
+  async function fetchData(id) {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/posts/' + id);
+
+    // Simpan di tempat yang disediakan
+    setBlogData(res.data);
+  }
+
+  // Sebelum ambil data, set status menjadi loading sehingga kita bisa memunculkan loading component
+  // Jika data berhasil didapat maka set success menjadi true, dan juga kebalikannya.
+  async function updateDataToServer(title, body, id) {
+    setStatus({
+      ...status,
+      loading: true,
+    })
+
+    // console.log(status);
+
+    const res = await axios.put('https://jsonplaceholder.typicode.com/posts/' + id, {
+      title: title,
+      body: body,
+      id: id,
+    });
+
+    console.log(res.status);
+
+    if (res.status !== 200) {
+      setStatus({
+        ...status,
+        loading: false,
+        success: false,
+      });
+      return;
+    }
+
+    setStatus({
+      ...status,
+      loading: false,
+      success: true,
+    });
+  }
+
+
+
+  // Sebelum form disubmit, cancel event form dan isi event dengan fungsi yang kita buat.
+  function onSubmitForm(event) {
+    event.preventDefault();
+    updateDataToServer(title, body, id);
+  }
+
+  // Ketika ada perubahan pada id maka ambil data dan simpan
+  useEffect(() => {
+    if (id) {
+      fetchData(id);
+    }
+  }, [id]);
+
+  // Loading component ketika status menjadi loading
+  if (status.loading) {
+    return <p>Loading</p>;
+  }
+
+  let statusEdit = status.success ? 'berhasil' : '';
+
+  console.log('status-success: ' + status.success);
+
+
   return (
-    <Authenticated>
-      <p>PUT url:https://jsonplaceholder.typicode.com/posts/1</p>
-      <p>Edit Ya</p>
-    </Authenticated>
-  );  
+    <>
+      <Authenticated>
+
+
+        {/* Render single data*/}
+        <p>Edit Data From: Id {id}</p>
+        <h2>Title: </h2>
+        <p>{blogData.title}</p>
+        <h2>Body: </h2>
+        <p>{blogData.body}</p>
+
+        {/* <p>PUT url:https://jsonplaceholder.typicode.com/posts/1</p>
+        <p>Edit Ya</p> */}
+
+        <p>To</p>
+
+
+        <form onSubmit={onSubmitForm}>
+          <h3>Edit Blog</h3>
+          <p>Title</p>
+          <input
+            placeholder="title here"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+          <p>Body</p>
+          <input
+            placeholder="body here"
+            value={body}
+            onChange={(event) => setBody(event.target.value)}
+          />
+          <br />
+          <br />
+
+          <br />
+          <button type="submit">
+            Save
+        </button>
+          <br /><br />
+          <p>Status edit: {statusEdit}</p>
+        </form>
+
+      </Authenticated>
+    </>
+  );
 }
